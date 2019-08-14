68e2473b2f1b2e6ed9fbe37d1608ff88b07e7035
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-08-14 20:47:18 -0300

    c

--- LAB01/Erros.m
+++ LAB01/Erros.m
@@ -1,9 +1,5 @@
-function valor = Erros(p)
+function valor = Erros(numeroBits,bitInformacaoPalavra,q,p,seed)
 
-seed = 10;
-numeroBits = 1000000;
-bitInformacaoPalavra = 4;
-q = 0.5;
 V = geradorMatrizBits(numeroBits,bitInformacaoPalavra,q,seed);
 V = geradorMatrizPalavra4bits(V);
 R = geradorMatrizRecebida(V,p,seed);
--- LAB01/teste.m
+++ LAB01/teste.m
@@ -1,7 +1,10 @@
-p = linspace(0.5,0,11);
+p = linspace(0.5,0,20);
 pb = zeros(1,size(p,2));
-a = 1000000;
+seed = 10;
+numeroBits = 1000000;
+bitInformacaoPalavra = 4;
+q = 0.5;
 for i = 1 : size(p,2)
-    pb(i) = Erros(p(i))/a;
+    pb(i) = Erros(numeroBits,bitInformacaoPalavra,q,p(i),seed)/numeroBits;
 end
 plot(p,pb);
