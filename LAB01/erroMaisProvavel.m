a8b3c1e0a6784f4aa7b2a76256be696059e58b5f
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-08-14 21:01:10 -0300

    a

--- LAB01/erroMaisProvavel.m
+++ LAB01/erroMaisProvavel.m
@@ -1,13 +1,15 @@
 function e = erroMaisProvavel(s,Ht,r)
    E = eye(7);
+   vec3Zeros = zeros(1,3);
+   vec7Zeros = zeros(1,7);   
    for i = 1:7
        
        x = rem(E(i,:)*Ht,2);
        
-       if(isequal(x,s) && isequal(rem(xor(E(i,:),r)*Ht,2),zeros(1,3)))
+       if(isequal(x,s) && isequal(rem(xor(E(i,:),r)*Ht,2),vec3Zeros))
            e = E(i,:);
        else
-           e = zeros(1,7);
+           e = vec7Zeros;
        end
        
    end
