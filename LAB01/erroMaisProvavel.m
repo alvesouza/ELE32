5cf5e3a05787e2564a80c302757f40d94ab5962c
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-08-14 21:44:39 -0300

    1

--- LAB01/erroMaisProvavel.m
+++ LAB01/erroMaisProvavel.m
@@ -1,15 +1,14 @@
 function e = erroMaisProvavel(s,Ht,r)
    E = eye(7);
    vec3Zeros = zeros(1,3);
-   vec7Zeros = zeros(1,7);   
+   vec7Zeros = zeros(1,7);
+   e = vec7Zeros;
    for i = 1:7
        
        x = rem(E(i,:)*Ht,2);
        
        if(isequal(x,s) && isequal(rem(xor(E(i,:),r)*Ht,2),vec3Zeros))
            e = E(i,:);
-       else
-           e = vec7Zeros;
        end
        
    end
--- LAB01/teste.m
+++ LAB01/teste.m
@@ -1,10 +1,11 @@
-p = linspace(0.5,0,20);
+p = linspace(0.5,0,21);
 pb = zeros(1,size(p,2));
 seed = 10;
-numeroBits = 1000000;
+s = rng(seed);
+numeroBits = 4*10000;
 bitInformacaoPalavra = 4;
 q = 0.5;
 for i = 1 : size(p,2)
-    pb(i) = Erros(numeroBits,bitInformacaoPalavra,q,p(i),seed)/numeroBits;
+    pb(i) = Erros(numeroBits,bitInformacaoPalavra,q,p(i),randi([0,2^31]))/numeroBits;
 end
 plot(p,pb);
