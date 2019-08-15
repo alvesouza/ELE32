546693cdf932607737ee794f0ddeffab2c846fb6
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-08-14 21:53:06 -0300

    1

--- LAB01/erroMaisProvavel.m
+++ LAB01/erroMaisProvavel.m
@@ -3,14 +3,15 @@ function e = erroMaisProvavel(s,Ht,r)
    vec3Zeros = zeros(1,3);
    vec7Zeros = zeros(1,7);
    e = vec7Zeros;
-   for i = 1:7
+   i = 1;
+   while (i <= 7 && isequal(e, vec7Zeros))
        
        x = rem(E(i,:)*Ht,2);
        
        if(isequal(x,s) && isequal(rem(xor(E(i,:),r)*Ht,2),vec3Zeros))
            e = E(i,:);
        end
-       
+   i = i + 1;   
    end
 
 end
